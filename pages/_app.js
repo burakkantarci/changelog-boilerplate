6d13e180dffca036fdba94d46a29bd2eae21133f
Burak Kantarcı <burakkantarci@Buraks-MacBook-Pro.local>
2021-10-26 16:57:08 +0300

    added mixpanel

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,12 @@
 import 'tailwindcss/tailwind.css'
+import mixpanel from 'mixpanel-browser';
+// or with require() syntax:
+// const mixpanel = require('mixpanel-browser');
+
+// Enabling the debug mode flag is useful during implementation,
+// but it's recommended you remove it for production
+mixpanel.init('b1fe62e77d332c49f378bce11d03ef35', {debug: true}); 
+mixpanel.track('Sign up');
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
--- pages/profile/index.js
+++ pages/profile/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 
+
 export default function Profile() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2 bg-gray-50">
